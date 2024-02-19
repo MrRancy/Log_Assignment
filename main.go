bd4b1eca48dadfaf9048083a6436ba230ca6289d
Teja Surisetty <teja.surisetty@newstreettech.com>
2024-02-20 00:30:51 +0530

    Modifed retry to 3 overall

--- main.go
+++ main.go
@@ -27,10 +27,10 @@ func main() {
 	// Environment Variables
 	batchSize := utils.GetEnvAsInt("BATCH_SIZE", 5)
 	batchInterval := utils.GetEnvAsInt("BATCH_INTERVAL", 1)
-	postEndpoint := utils.GetEnv("POST_ENDPOINT", "https://httpdump.app/dumps/8e06d9f6-ac86-4629-a13b-f2d802a24fbe")
+	postEndpoint := utils.GetEnv("POST_ENDPOINT", "https://httpdump.app/dumps/8e06d9f6-ac86-4629-a13b-f2d802a24fbe2")
 
 	// Creating In Memory store
-	cache := store.NewCache(log, postEndpoint, batchInterval, batchSize, 3, 2)
+	cache := store.NewCache(log, postEndpoint, batchInterval, batchSize, 2, 2)
 	cache.InitializeLogMonitor()
 
 	// Initializing the Controller
